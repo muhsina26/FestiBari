47acab14a9e9bf2597ad36071bb58c107de62f80
lamializa991 <lamializa991@gmail.com>
2025-08-15 22:17:30 +0600

    Profile dropdown functionality added

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -8,23 +8,23 @@
         <li><a href="{{ url('/submit') }}">Submit</a></li>
         <li><a href="{{ url('/contact') }}">Contact</a></li>
 
-        @guest
-            <li id="login-item"><a href="{{ route('login') }}" class="auth-link">Login</a></li>
-            <li id="signup-item"><a href="{{ route('register') }}" class="auth-link signup-link">Sign Up</a></li>
-        @else
-            <li class="profile-dropdown" id="profile-item">
+       @auth
+            <li class="profile-dropdown">
                 <a href="#" class="profile-icon">
                     <i class="fas fa-user-circle"></i>
                 </a>
                 <div class="dropdown-content">
                     <span class="welcome-msg">Welcome, {{ Auth::user()->name }}</span>
                     <a href="#" class="dropdown-item">Profile</a>
-                    <form action="{{ route('logout') }}" method="POST">
+                    <form method="POST" action="{{ route('logout') }}" style="margin: 0;">
                         @csrf
-                        <button type="submit" id="logout-button" class="dropdown-item">Logout</button>
+                        <button type="submit" class="dropdown-item">Logout</button>
                     </form>
                 </div>
             </li>
-        @endguest
+        @else
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('register') }}" class="signup-link">Sign Up</a></li>
+        @endauth
     </ul>
 </nav>
