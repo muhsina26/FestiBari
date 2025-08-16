3bf1e96c180afecab7e869a4c4b745ab1b08cb6a
MimAtia <mim15-5375@diu.edu.bd>
2025-08-16 23:50:58 +0600

    Form submission implemented

--- database/migrations/2025_08_16_165336_create_festivals_table.php
+++ database/migrations/2025_08_16_165336_create_festivals_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('festivals', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->string('district');
+            $table->string('area')->nullable();
+            $table->text('full_address')->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->string('religion');
+            $table->string('image_path')->nullable();
+            $table->json('subevents')->nullable();
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
