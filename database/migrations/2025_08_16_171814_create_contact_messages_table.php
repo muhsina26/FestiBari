a9245d26d5119a17e5c0fa425320731981dbc40e
MimAtia <mim15-5375@diu.edu.bd>
2025-08-16 23:45:58 +0600

    Mim:Contact backend implemented

--- database/migrations/2025_08_16_171814_create_contact_messages_table.php
+++ database/migrations/2025_08_16_171814_create_contact_messages_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('contact_messages', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('subject');
+            $table->text('message');
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
         });
     }
